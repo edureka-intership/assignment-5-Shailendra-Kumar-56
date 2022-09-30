1832409ac08179183188bde0b1f659a613586e57
Shailendra <sail.like@gmail.com>
2022-10-01 00:15:05 +0530

    Update locations.js

--- controllers/locations.js
+++ controllers/locations.js
@@ -1,4 +1,4 @@
-const Locations = require('../Models/location.json');
+const Locations = require('../models/location.json');
 
 exports.getLocations = (req, res) => {
     res.status(200).json({message:"Locations Fetched Successfully", locations: Locations})
